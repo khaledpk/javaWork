573cfbb689a65f66fb1c9dce9e594390f5560ddd
khaledpk <93967045+khaledpk@users.noreply.github.com>
2021-11-29 16:37:35 +0300

    work1

---
+++ addsub.java
@@ -0,0 +1,19 @@
+class Myaddsub{
+    
+
+    public void getAdd(Double c,int d){
+        
+        System.out.println("output area of right triangle: "+(c*d)/2);
+      
+    }
+
+    public void getAdd(Double c,int d,Double e){
+        
+       
+        System.out.println("output of area of heroi triangle: "+(c*d*e));
+}
+
+}
+
+
+
